c4bb7ff0bd9ee6c06707739cbbac1bfc324deb3f
paulotasso7 <paulotasso7@gmail.com>
2022-06-08 22:24:25 -0300

    adding google-recaptcha to front end

--- src/App.js
+++ src/App.js
@@ -1,160 +1,170 @@
-import React, { Component } from 'react';
-import Particles from 'react-particles-js';
+import React, { Component } from "react";
+import Particles from "react-particles-js";
 
-
-import './App.css';
-import Navigation from './components/Navigation/Navigation'; 
-import ImgLinkForm from './components/ImgLinkForm/ImgLinkForm'
-import Logo from './components/Logo/Logo';
-import Rank from './components/Rank/Rank'
-import FaceReckon from './components/FaceReckon/FaceReckon'; 
-import Signin from './components/Signin/Signin';
-import Register from './components/Register/Register';
-
+import "./App.css";
+import Navigation from "./components/Navigation/Navigation";
+import ImgLinkForm from "./components/ImgLinkForm/ImgLinkForm";
+import Logo from "./components/Logo/Logo";
+import Rank from "./components/Rank/Rank";
+import FaceReckon from "./components/FaceReckon/FaceReckon";
+import Signin from "./components/Signin/Signin";
+import Register from "./components/Register/Register";
 
 const particlesOptions = {
   particles: {
     number: {
-      value: 49
+      value: 49,
     },
     line_linked: {
-      distance: 198
-    }    
+      distance: 198,
+    },
   },
   interactivity: {
-    "detect_on":"window",
-    "events":{
-      "onhover":{
-        "enable":true,
-        "mode":"react"
+    detect_on: "window",
+    events: {
+      onhover: {
+        enable: true,
+        mode: "react",
       },
-      "onclick":{
-        "enable":true,
-        "mode":"push"
-      }
-    }
-  }      
-}
+      onclick: {
+        enable: true,
+        mode: "push",
+      },
+    },
+  },
+};
 
 const initialState = {
-    input: '',
-    imgURL:'',
-    box: {},
-    route: 'signin',
-    isSignedIn: false,
-    user: {
-        id: '',
-        name: '',
-        email: '',
-        entries: 0,
-        joined: ''
-    }
+  input: "",
+  imgURL: "",
+  box: {},
+  route: "signin",
+  isSignedIn: false,
+  user: {
+    id: "",
+    name: "",
+    email: "",
+    entries: 0,
+    joined: "",
+  },
 };
 
 class App extends Component {
-    constructor() {
-      super();
-        this.state = initialState
-    }
+  constructor() {
+    super();
+    this.state = initialState;
+  }
 
-    loadUser = (data) => {
-        this.setState({user: {
-            id: data.id,
-            name: data.name,
-            email: data.email,
-            entries: data.entries,
-            joined: data.joined
-        }})
-    }
+  loadUser = (data) => {
+    this.setState({
+      user: {
+        id: data.id,
+        name: data.name,
+        email: data.email,
+        entries: data.entries,
+        joined: data.joined,
+      },
+    });
+  };
 
-    calculateFaceLocation = (data) => {
-        const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
-        const image = document.getElementById('inputimage');
-        const width = Number(image.width);
-        const height = Number(image.height);
-            return {
-                leftCol: clarifaiFace.left_col * width,
-                topRow: clarifaiFace.top_row * width,
-                rightCol: width - (clarifaiFace.right_col * width),
-                bottomRow: height - (clarifaiFace.bottom_row * height)
-            }   
-    } 
+  calculateFaceLocation = (data) => {
+    const clarifaiFace =
+      data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById("inputimage");
+    const width = Number(image.width);
+    const height = Number(image.height);
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * width,
+      rightCol: width - clarifaiFace.right_col * width,
+      bottomRow: height - clarifaiFace.bottom_row * height,
+    };
+  };
 
-    displayFaceBox = (box) => {
-        this.setState({box: box});
-    }
+  displayFaceBox = (box) => {
+    this.setState({ box: box });
+  };
 
-    onInputChange = (event) => {
-        this.setState({input: event.target.value});
-    }
+  onInputChange = (event) => {
+    this.setState({ input: event.target.value });
+  };
 
-    onButtonSubmit = () => {  
-        this.setState({imgURL: this.state.input});
-        fetch('https://blooming-anchorage-07854.herokuapp.com/imageurl', {
-            method: 'post',
-            headers: {'Content-Type': 'application/json'},
+  onButtonSubmit = () => {
+    this.setState({ imgURL: this.state.input });
+    fetch("https://blooming-anchorage-07854.herokuapp.com/imageurl", {
+      method: "post",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        input: this.state.input,
+      }),
+    })
+      .then((res) => res.json())
+      .then((response) => {
+        if (response) {
+          fetch("https://blooming-anchorage-07854.herokuapp.com/image", {
+            method: "put",
+            headers: { "Content-Type": "application/json" },
             body: JSON.stringify({
-                input: this.state.input
+              id: this.state.user.id,
+            }),
+          })
+            .then((res) => res.json())
+            .then((count) => {
+              this.setState(Object.assign(this.state.user, { entries: count }));
             })
-        })
-        .then(res => res.json())
-        .then((response) => { 
-            if(response) {
-                fetch('https://blooming-anchorage-07854.herokuapp.com/image', {
-                    method: 'put',
-                    headers: {'Content-Type': 'application/json'},
-                    body: JSON.stringify({
-                        id: this.state.user.id
-                    })
-                })
-                .then(res => res.json())
-                .then(count => {
-                    this.setState(Object.assign(this.state.user, { entries: count}))
-                })
-                .catch(console.log)
-            }
-        this.displayFaceBox(this.calculateFaceLocation(response));
-        })
-        .catch(err => console.log(err));
-    }
-  
-    onRouteChange = (route) => {
-        if (route === 'signout') {
-        this.setState(initialState)
-        } else if (route === 'home') {
-        this.setState({isSignedIn: true})
+            .catch(console.log);
         }
-        this.setState({route: route});
-    }
+        this.displayFaceBox(this.calculateFaceLocation(response));
+      })
+      .catch((err) => console.log(err));
+  };
 
-    render() {
-        const {isSignedIn, route, box, imgURL} = this.state;  
-        return (
-            <div className="App">
-                <Particles 
-                className='particles'
-                params={particlesOptions}
-                />
-                <Navigation isSignedIn={isSignedIn} onRouteChange= { this.onRouteChange }/>
-                { route === 'home'
-                ?   <div>
-                        <Logo />
-                        <Rank name={this.state.user.name} entries={this.state.user.entries}/>
-                        <ImgLinkForm onInputChange= {this.onInputChange} onButtonSubmit= {this.onButtonSubmit} />
-                        <FaceReckon 
-                        box= {box}
-                        imgURL= {imgURL}
-                        />
-                    </div>
-                : ( 
-                route === 'signin'
-                ? <Signin onRouteChange= {this.onRouteChange} loadUser={this.loadUser}/>
-                : <Register onRouteChange= {this.onRouteChange} loadUser={this.loadUser}/>
-                )
-                } 
-            </div>
-        );
+  onRouteChange = (route) => {
+    if (route === "signout") {
+      this.setState(initialState);
+    } else if (route === "home") {
+      this.setState({ isSignedIn: true });
     }
-};
+    this.setState({ route: route });
+  };
+
+  render() {
+    const { isSignedIn, route, box, imgURL } = this.state;
+    return (
+      <div className="App">
+        <Particles className="particles" params={particlesOptions} />
+        <Navigation
+          isSignedIn={isSignedIn}
+          onRouteChange={this.onRouteChange}
+        />
+        {route === "home" ? (
+          <div>
+            <Logo />
+            <Rank
+              name={this.state.user.name}
+              entries={this.state.user.entries}
+            />
+            <ImgLinkForm
+              onInputChange={this.onInputChange}
+              onButtonSubmit={this.onButtonSubmit}
+            />
+            <FaceReckon box={box} imgURL={imgURL} />
+          </div>
+        ) : route === "signin" ? (
+          <Signin onRouteChange={this.onRouteChange} loadUser={this.loadUser} />
+        ) : (
+          <Register
+            onRouteChange={this.onRouteChange}
+            loadUser={this.loadUser}
+          />
+        )}
+      </div>
+    );
+  }
+}
 
 export default App;
+
+// SITE_KEY: 6LcHxVAgAAAAAMFi1y3HPdNNMFqYYRESX0Lq4Wh1
+
+// SECRET_SITE_KEY: 6LcHxVAgAAAAAPvOVl5Bx9wSnXtKxjW93vmsQ6GB
--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -1,91 +1,111 @@
 import React from "react";
+import ReCAPTCHA from "react-google-recaptcha";
 
-class Signin extends React.Component  {
-    constructor(props) {
-        super(props);
-        this.state= {
-            signInEmail : '',
-            signInPassword: ''
+class Signin extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      signInEmail: "",
+      signInPassword: "",
+      checkedRecaptcha: false,
+    };
+  }
+
+  onEmailChange = (event) => {
+    this.setState({ signInEmail: event.target.value });
+  };
+
+  onPasswordChange = (event) => {
+    this.setState({ signInPassword: event.target.value });
+  };
+
+  onSubmitSignIn = () => {
+    fetch("https://blooming-anchorage-07854.herokuapp.com/signin", {
+      method: "post",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        email: this.state.signInEmail,
+        password: this.state.signInPassword,
+      }),
+    })
+      .then((res) => res.json())
+      .then((user) => {
+        if (user.id) {
+          this.props.loadUser(user);
+          this.props.onRouteChange("home");
+        } else {
+          alert("Dados incorretos");
         }
-    }
+      });
+  };
 
-    onEmailChange = (event) => {
-        this.setState({signInEmail: event.target.value})
-    }
+  onRecaptchaChange = () => {
+    this.setState({ checkedRecaptcha: true });
+  };
 
-    onPasswordChange = (event) => {
-        this.setState({signInPassword: event.target.value})
-    }
-
-    onSubmitSignIn = () => {
-        fetch('https://blooming-anchorage-07854.herokuapp.com/signin', {
-            method: 'post',
-            headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify({
-                email: this.state.signInEmail,
-                password: this.state.signInPassword
-            })
-        })
-        .then(res => res.json())
-        .then( user => {
-            if(user.id){
-                this.props.loadUser(user);
-                this.props.onRouteChange('home');
-            } else {
-                alert('Dados incorretos')
-            }
-        })
-        
-    }    
-
-    render() {
-        const {onRouteChange} = this.props;
-        return (
-            <article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
-                <main className="pa4 black-80">
-                    <div className="measure">
-                        <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
-                            <legend className="f1 br3 fw6 ph0 mh0">Sign In</legend>
-                            <div className="mt3">
-                                <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
-                                <input 
-                                onChange={this.onEmailChange}
-                                className="pa2 input-reset br2 ba bg-transparent hover-bg-black hover-white w-100" 
-                                type="email" 
-                                name="email-address"  
-                                id="email-address"
-                                />
-                            </div>
-                            <div className="mv3">
-                                <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
-                                <input 
-                                onChange={this.onPasswordChange}
-                                className="b pa2 input-reset br2 ba bg-transparent hover-bg-black hover-white w-100" 
-                                type="password" 
-                                name="password"  
-                                id="password"
-                                />
-                            </div>
-                        </fieldset>
-                        <div className="">
-                            <input 
-                            onClick= {this.onSubmitSignIn}
-                            className="b ph3 pv2 input-reset br2 ba b--black bg-transparent grow pointer f6 dib" 
-                            type="submit" 
-                            value="Sign in"
-                            />
-                        </div>
-                        <div className="lh-copy mt3">
-                            <p onClick={() => onRouteChange('register')} 
-                            className="f6 link dim black  pointer db">Register</p>
-                        </div>
-                    </div>
-                </main>
-            </article>
-        );
-    }
-        
-    
+  render() {
+    const { onRouteChange } = this.props;
+    return (
+      <article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
+        <main className="pa4 black-80">
+          <div className="measure">
+            <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
+              <legend className="f1 br3 fw6 ph0 mh0">Sign In</legend>
+              <div className="mt3">
+                <label className="db fw6 lh-copy f6" htmlFor="email-address">
+                  Email
+                </label>
+                <input
+                  onChange={this.onEmailChange}
+                  className="pa2 input-reset br2 ba bg-transparent hover-bg-black hover-white w-100"
+                  type="email"
+                  name="email-address"
+                  id="email-address"
+                />
+              </div>
+              <div className="mv3">
+                <label className="db fw6 lh-copy f6" htmlFor="password">
+                  Password
+                </label>
+                <input
+                  onChange={this.onPasswordChange}
+                  className="b pa2 input-reset br2 ba bg-transparent hover-bg-black hover-white w-100"
+                  type="password"
+                  name="password"
+                  id="password"
+                />
+              </div>
+            </fieldset>
+            <div className="">
+              <ReCAPTCHA
+                style={{ marginTop: "-15px" }}
+                sitekey="6Lc5fFUgAAAAAOhWhbI9g-wg5P8h89z02ckXScIG"
+                onChange={this.onRecaptchaChange}
+              />
+              <input
+                onClick={() => {
+                  if (!this.state.checkedRecaptcha)
+                    return alert("Please, check the recaptcha checkbox");
+                  this.onSubmitSignIn();
+                }}
+                className=" mt3 b ph3 pv2 input-reset br2 ba b--black bg-transparent grow pointer f6 dib"
+                type="submit"
+                value="Sign in"
+              />
+            </div>
+            <div className="lh-copy mt3">
+              <p
+                onClick={() => onRouteChange("register")}
+                className="f6 link dim black  pointer db"
+              >
+                Register
+              </p>
+            </div>
+          </div>
+        </main>
+      </article>
+    );
+  }
 }
 
 export default Signin;
