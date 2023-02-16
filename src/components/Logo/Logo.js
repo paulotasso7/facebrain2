a8ff618b6b7b5457357ade5492b570ff61e6474e
paulo.costa <202204061538@alunos.estacio.br>
2023-02-16 10:18:17 -0300

    fixing dependency tree for create react app scripts

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,5 +1,4 @@
 import React from "react";
-import Tilt from 'react-tilt';
 import brain from './brain.png'
 
 import './Logo.css';
@@ -8,11 +7,7 @@ import './Logo.css';
 const Logo = () => {
     return (
         <div className= 'ma4 mt0' >
-            <Tilt className="Tilt br2 shadow-2" options={{ max : 55 }} style={{ height: 150, width: 150 }} >
-                <div className="Tilt-inner pa3"> 
-                    <img style={{paddingTop: '5px'}}alt='brainLogo' src={ brain }/> 
-                </div>
-            </Tilt>
+            <img style={{paddingTop: '5px'}}alt='brainLogo' src={ brain }/>
         </div>
     );
 }
